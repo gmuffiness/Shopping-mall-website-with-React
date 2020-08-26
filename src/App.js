61569db0e72424e339d8dd09e3025cce2611fc18
gmuffiness <35298963+gmuffiness@users.noreply.github.com>
2020-08-26 23:29:57 +0900

    [setting & update] PORT changed to 3001, navbar update

--- src/App.js
+++ src/App.js
@@ -21,6 +21,7 @@ function App() {
           <Nav className="mr-auto">
             <Nav.Link as={Link} to="/products">Products</Nav.Link>
             <Nav.Link as={Link} to="/detail/">Detail</Nav.Link>
+            <Nav.Link as={Link} to="/cart/">Cart</Nav.Link>
             <NavDropdown title="Dropdown" id="basic-nav-dropdown">
               <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
